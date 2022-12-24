0e009e4e591a6cbdd36c4cac616d7949bcbdfe6a
Robert Coffie <rcoffie22@yahoo.com>
2022-12-24 21:05:55 +0000

    adding django humanize

--- devSo/settings.py
+++ devSo/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # django inbuild apps
+    'django.contrib.humanize',
+    # apps
     'projects',
 ]
 
