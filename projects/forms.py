88fa9620588de79a64afa82c23ec7edf08fc2fd3
Robert Coffie <rcoffie22@yahoo.com>
2022-12-31 11:33:33 +0000

    create project form refactor

--- projects/forms.py
+++ projects/forms.py
@@ -5,4 +5,28 @@ from projects.models import Project
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = '__all__'
+        fields = ['title', 'description', 'demo_link',
+                  'source_link', 'tags', 'vote_total', 'ratio']
+
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        self.fields['title'].widget.attrs.update(
+            {'class': 'input', 'placeholder': 'Title'}
+        )
+
+        self.fields['description'].widget.attrs.update(
+            {'class': 'input', 'placeholder': 'Description (optional)'}
+        )
+
+        self.fields['demo_link'].widget.attrs.update(
+            {'class': 'input', 'placeholder': 'link'}
+        )
+
+        self.fields['source_link'].widget.attrs.update(
+            {'class': 'input', 'placeholder': 'source link'}
+        )
