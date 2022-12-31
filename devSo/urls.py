47ba9b3b3571233ff3dfce7cbfc1cc1533a23b04
Robert Coffie <rcoffie22@yahoo.com>
2022-12-31 01:03:23 +0000

    adding index page layout

--- devSo/urls.py
+++ devSo/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('projects.urls'))
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', include('projects.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
