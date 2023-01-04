e4642c681794d0ece21955be8330b9048831626e
Robert Coffie <rcoffie22@yahoo.com>
2023-01-04 14:09:36 +0000

    fixing user registration bug

--- users/forms.py
+++ users/forms.py
@@ -9,10 +9,3 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User 
         fields = ('username','email','password1','password2')
-
-    def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-            return user
